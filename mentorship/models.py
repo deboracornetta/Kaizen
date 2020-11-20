8eac0aa0143bfb9d1ae4cbdf5cc23bbd7f8c97ca
Debora Cornetta <cornettaintech@gmail.com>
2020-11-20 15:29:48 -0300

    Students model created and superuser active

--- mentorship/models.py
+++ mentorship/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=100)
+    courses = models.TextField()
+    teacher = models.ForeignKey(User, null=True, on_delete=models.SET_NULL) 
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
--- mentorship/views.py
+++ mentorship/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
+from .models import Student
 
 
 def home(request):
     return render(request, 'mentorship/home.html')
 
 def about(request):
-    
+
     return render(request, 'mentorship/about.html')
